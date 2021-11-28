3918ee8f184cc4f7bbd46c114d2dabe4833fa444
NexGenHuman <konradwasilewski@vp.pl>
2021-11-28 18:30:57 +0100

    Example data entered into the database

--- react_project/src/Classes/Screening.js
+++ react_project/src/Classes/Screening.js
@@ -1,10 +1,10 @@
 export default class Screening {
-    constructor (date, startTime, movie, screeningRoom, soldTicketsNumber, takenSeats, id) {
+    constructor (date, startTime, movieID, screeningRoomNum, soldTicketsNumber, takenSeats, id) {
         this.id = id;
         this.date = date;
         this.startTime = startTime;
-        this.movie = movie;
-        this.screeningRoom = screeningRoom;
+        this.movieID = movieID;
+        this.screeningRoomNum = screeningRoomNum;
         this.soldTicketsNumber = soldTicketsNumber;
         this.availableTicketsNumber = screeningRoom.capacity - soldTicketsNumber;
         this.takenSeats = takenSeats;
