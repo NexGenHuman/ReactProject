dc5fe3e58b513d0977e5bd4ab8d42375c7c2c149
NexGenHuman <konradwasilewski@vp.pl>
2021-11-28 18:15:28 +0100

    Rest of http meethods added

--- react_project/src/Server/server.js
+++ react_project/src/Server/server.js
@@ -191,6 +191,111 @@ app.get('/screenings/:id', (req, res) => {
     });
 });
 
+app.post('/screenings', (req, res) => {
+    fs.readFile('./JSON/screenings.json', 'utf8', (err, screeningsJson) => {
+        if (err) {
+            console.log("File read failed in GET /screenings/" + req.params.id + ": "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var screenings = JSON.parse(screeningsJson);
+        var screening = screenings.find(screeningtmp => screeningtmp.id == req.params.id);
+        if (!screening) {
+            screenings.push(req.body);
+            var newList = JSON.stringify(screenings);
+            fs.writeFile('./JSON/screenings.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in POST /screenings: "+ err);
+                    res.status(500).send('Error writing file screenings.json');
+                } else {
+                    res.status(201).send(req.body);
+                    console.log("Successfully wrote file screenings.json and added new screening with id = " + req.body.id);
+                }
+            });
+        } else {
+            console.log("Screening by id = " + req.body.id + " already exists");
+            res.status(500).send('Screening by id = ' + req.body.id + ' already exists');
+            return;
+        }
+    });
+});
+
+app.put('/screenings/:id', (req, res) => {
+    fs.readFile('./JSON/screenings.json', 'utf8', (err, screeningsJson) => {
+        if (err) {
+            console.log("File read failed in PUT /screenings/" + req.params.id+": "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var screenings = JSON.parse(screeningsJson);
+        var screeningBody = screenings.find(screeningtmp => screeningtmp.id == req.body.id);
+        if (screeningBody && screeningBody.id != req.params.id) {
+            console.log("Screening by id = " + screeningBody.id + " already exists");
+            res.status(500).send('Screening by id = ' + screeningBody.id + ' already exists');
+            return;
+        }
+        var screening = screenings.find(screeningtmp => screeningtmp.id == req.params.id);
+        if (!screening) {
+            screenings.push(req.body);
+            var newList = JSON.stringify(screenings);
+            fs.writeFile('./JSON/screenings.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in PUT /screenings/" + req.params.id+": "+err);
+                    res.status(500).send('Error writing file screenings.json');
+                } else {
+                    res.status(201).send(req.body);
+                    console.log("Successfully wrote file screenings.json and added new screening with id = " + req.body.screeningId);
+                }
+            });
+        } else {
+            for (var i = 0; i < screenings.length; i++) {
+                if (screenings[i].id == screening.id) {
+                    screenings[i] = req.body;
+                }
+            }
+            var newList = JSON.stringify(screenings);
+            fs.writeFile('./JSON/screenings.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in PUT /screenings/" + req.params.id+": "+ err);
+                    res.status(500).send('Error writing file screenings.json');
+                } else {
+                    res.status(200).send(req.body);
+                    console.log("Successfully wrote file screenings.json and edit screening with old id = " + req.params.id);
+                }
+            });
+        }
+    });
+});
+
+app.delete('/screenings/:id', (req, res) => {
+    fs.readFile('./JSON/screenings.json', 'utf8', (err, screeningsJson) => {
+        if (err) {
+            console.log("File read failed in DELETE /screenings: "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var screenings = JSON.parse(screeningsJson);
+        var screeningIndex = screenings.findIndex(screeningtmp => screeningtmp.id == req.params.id);
+        if (screeningIndex != -1) {
+            screenings.splice(screeningIndex, 1);
+            var newList = JSON.stringify(screenings);
+            fs.writeFile('./JSON/screenings.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in DELETE /screenings/" + req.params.id + ": " + err);
+                    res.status(500).send('Error writing file screenings.json');
+                } else {
+                    res.status(204).send();
+                    console.log("Successfully deleted screening with id = " + req.params.id);
+                }
+            });
+        } else {
+            console.log("Screening by id = " + req.params.id + " does not exists");
+            res.status(500).send('Screening by id = ' + req.params.id + ' does not exists');
+            return;
+        }
+    });
+});
+
 //-----------------------SCREENING ROOMS
 
 app.get('/screeningRooms', (req, res) => {
@@ -222,6 +327,111 @@ app.get('/screeningRooms/:number', (req, res) => {
         var screeningRoomJSON = JSON.stringify(screeningRoom);
         console.log("GET /screeningRooms/" + req.params.number);
         res.send(screeningRoomJSON);
+    });
+});
+
+app.post('/screeningRooms', (req, res) => {
+    fs.readFile('./JSON/screeningRooms.json', 'utf8', (err, screeningRoomsJson) => {
+        if (err) {
+            console.log("File read failed in GET /screeningRooms/" + req.params.number + ": "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var screeningRooms = JSON.parse(screeningRoomsJson);
+        var screeningRoom = screeningRooms.find(screeningRoomtmp => screeningRoomtmp.number == req.params.number);
+        if (!screeningRoom) {
+            screeningRooms.push(req.body);
+            var newList = JSON.stringify(screeningRooms);
+            fs.writeFile('./JSON/screeningRooms.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in POST /screeningRooms: "+ err);
+                    res.status(500).send('Error writing file screeningRooms.json');
+                } else {
+                    res.status(201).send(req.body);
+                    console.log("Successfully wrote file screeningRooms.json and added new screening room with number = " + req.body.number);
+                }
+            });
+        } else {
+            console.log("Screening room by number = " + req.body.number + " already exists");
+            res.status(500).send('Screening room by number = ' + req.body.number + ' already exists');
+            return;
+        }
+    });
+});
+
+app.put('/screeningRooms/:number', (req, res) => {
+    fs.readFile('./JSON/screeningRooms.json', 'utf8', (err, screeningRoomsJson) => {
+        if (err) {
+            console.log("File read failed in PUT /screeningRooms/" + req.params.number+": "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var screeningRooms = JSON.parse(screeningRoomsJson);
+        var screeningRoomBody = screeningRooms.find(screeningRoomtmp => screeningRoomtmp.number == req.body.number);
+        if (screeningRoomBody && screeningRoomBody.number != req.params.number) {
+            console.log("Screening room by number = " + screeningRoomBody.number + " already exists");
+            res.status(500).send('Screening room by number = ' + screeningRoomBody.number + ' already exists');
+            return;
+        }
+        var screeningRoom = screeningRooms.find(screeningRoomtmp => screeningRoomtmp.number == req.params.number);
+        if (!screeningRoom) {
+            screeningRooms.push(req.body);
+            var newList = JSON.stringify(screeningRooms);
+            fs.writeFile('./JSON/screeningRooms.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in PUT /screeningRooms/" + req.params.number+": "+err);
+                    res.status(500).send('Error writing file screeningRooms.json');
+                } else {
+                    res.status(201).send(req.body);
+                    console.log("Successfully wrote file screeningRooms.json and added new screening room with number = " + req.body.screeningRoomnumber);
+                }
+            });
+        } else {
+            for (var i = 0; i < screeningRooms.length; i++) {
+                if (screeningRooms[i].number == screeningRoom.number) {
+                    screeningRooms[i] = req.body;
+                }
+            }
+            var newList = JSON.stringify(screeningRooms);
+            fs.writeFile('./JSON/screeningRooms.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in PUT /screeningRooms/" + req.params.number + ": " + err);
+                    res.status(500).send('Error writing file screeningRooms.json');
+                } else {
+                    res.status(200).send(req.body);
+                    console.log("Successfully wrote file screeningRooms.json and edit screening room with old number = " + req.params.number);
+                }
+            });
+        }
+    });
+});
+
+app.delete('/screeningRooms/:number', (req, res) => {
+    fs.readFile('./JSON/screeningRooms.json', 'utf8', (err, screeningRoomsJson) => {
+        if (err) {
+            console.log("File read failed in DELETE /screeningRooms: "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var screeningRooms = JSON.parse(screeningRoomsJson);
+        var screeningRoomIndex = screeningRooms.findIndex(screeningRoomtmp => screeningRoomtmp.number == req.params.number);
+        if (screeningRoomIndex != -1) {
+            screeningRooms.splice(screeningRoomIndex, 1);
+            var newList = JSON.stringify(screeningRooms);
+            fs.writeFile('./JSON/screeningRooms.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in DELETE /screeningRooms/" + req.params.number + ": " + err);
+                    res.status(500).send('Error writing file screeningRooms.json');
+                } else {
+                    res.status(204).send();
+                    console.log("Successfully deleted screening room with number = " + req.params.number);
+                }
+            });
+        } else {
+            console.log("Screening room by number = " + req.params.number + " does not exists");
+            res.status(500).send('Screening room by number = ' + req.params.number + ' does not exists');
+            return;
+        }
     });
 });
 
