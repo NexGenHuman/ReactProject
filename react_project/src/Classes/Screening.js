c0dfc17dda5f04251d0ee28b82eef1196911c4e5
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-11-29 01:18:12 +0100

    screening constructor fix

--- react_project/src/Classes/Screening.js
+++ react_project/src/Classes/Screening.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class Screening {
     constructor (date, startTime, movieID, screeningRoomNum, soldTicketsNumber, takenSeats, id) {
         //Unique ID number of a movie
@@ -13,7 +15,11 @@ export default class Screening {
         //Number of sold tickets
         this.soldTicketsNumber = soldTicketsNumber;
         //Number of tickets available for sale
-        this.availableTicketsNumber = screeningRoom.capacity - soldTicketsNumber;
+        this.availableTicketsNumber = (available) => {
+            let screeningRoomJSON = axios.get('http://localhost:7777/screeningRooms/' + this.screeningRoomNum).then(response => console.log(response.data));
+            let screeningRoom = JSON.parse(screeningRoomJSON);
+            available = screeningRoom.capacity - soldTicketsNumber;
+        }
         //Array containing numbers of taken seats
         this.takenSeats = takenSeats;
     }
