deb5a595615c4ac4c2fe399c0c3c11f5a603709c
NexGenHuman <konradwasilewski@vp.pl>
2021-12-05 05:35:48 +0100

    Improved server side validation

--- react_project/src/Server/Validators/screeningValidator.js
+++ react_project/src/Server/Validators/screeningValidator.js
@@ -1,54 +1,61 @@
 export default function screeningValidator(screening) {
-    if(typeof screening.id !== 'number' || screening.id < 0) {
+    if (typeof screening.id !== 'number' || screening.id < 0) {
         console.log('Invalid screening/id value in request body');
-        return false; 
-        }
+        return false;
+    }
+    if (screening.date == '') {
+        console.log('Empty date in request');
+        return false;
+    }
     let date = new Date(screening.date);
-    if(!date) {
+    if (!date) {
         console.log('Invalid screening/date value in request body');
-        return false; 
-        }
+        return false;
+    }
+    if (screening.startTime == '') {
+        console.log('Empty date in request');
+        return false;
+    }
     date = new Date(screening.date + 'T' + screening.startTime);
-    if(!date) {
+    if (!date) {
         console.log('Invalid screening/time value in request body');
-        return false; 
-        }
-    if(date < new Date()) {
+        return false;
+    }
+    if (date <= new Date()) {
         console.log('Invalid screening/date-time precedes current date');
-        return false; 
-        }
-    if(typeof screening.movieID !== 'number' || screening.movieID < 0) {
+        return false;
+    }
+    if (typeof screening.movieID !== 'number' || screening.movieID < 0) {
         console.log('Invalid screening/movieID value in request body');
-        return false; 
-        }
-    if(typeof screening.screeningRoomNum !== 'number' || screening.screeningRoomNum < 0) {
+        return false;
+    }
+    if (typeof screening.screeningRoomNum !== 'number' || screening.screeningRoomNum < 0) {
         console.log('Invalid screening/screeningRoomNum value in request body');
-        return false; 
-        }
-    if(typeof screening.soldTicketsNumber !== 'number' || screening.soldTicketsNumber < 0) {
+        return false;
+    }
+    if (typeof screening.soldTicketsNumber !== 'number' || screening.soldTicketsNumber < 0) {
         console.log('Invalid screening/soldTicketsNumber value in request body');
-        return false; 
-        }
-    if(typeof screening.availableTicketsNumber !== 'number' || screening.availableTicketsNumber < 0) {
+        return false;
+    }
+    if (typeof screening.availableTicketsNumber !== 'number' || screening.availableTicketsNumber < 0) {
         console.log('Invalid screening/availableTicketsNumber value in request body');
-        return false; 
-        }
-    if(!Array.isArray(screening.takenSeats)) {
+        return false;
+    }
+    if (!Array.isArray(screening.takenSeats)) {
         console.log('Invalid screening/takenSeats value in request body');
-        return false; 
-        }
+        return false;
+    }
     let temp = true;
     var tempArr = [];
     screening.takenSeats.every(function (seatNum, index) {
         let tempSeat = tempArr.find(valtmp => valtmp === seatNum);
-        if(tempSeat)
-        {
+        if (tempSeat) {
             console.log('Value in screening/takenSeats/seatNum repeated');
             temp = false;
             return false;
         }
         tempArr.push(seatNum);
-        if(typeof seatNum !== 'number' || seatNum <= 0) {
+        if (typeof seatNum !== 'number' || seatNum <= 0) {
             console.log('Invalid screening/takenSeats/seatNum value in request body at index ' + index);
             temp = false;
             return false;
