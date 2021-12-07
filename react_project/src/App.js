44fd0773b4c9f4db7a289c6d3aa4a4ed293f7ff5
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-12-07 02:09:51 +0100

    Added low stylisation

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -10,7 +10,7 @@ import GetPopularity from './Components/GetPopularity';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App routerContainer">
       <Router>
         <div className="pageSelection">
           <p><Link to="/">Home</Link></p>
