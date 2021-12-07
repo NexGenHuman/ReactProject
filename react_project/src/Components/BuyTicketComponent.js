c0c155292bf05e3f8c83b15d5786a0b66821f2f4
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-12-07 21:58:34 +0100

    Tickets working

--- react_project/src/Components/BuyTicketComponent.js
+++ react_project/src/Components/BuyTicketComponent.js
@@ -8,14 +8,7 @@ export default class BuyTicketComponent extends React.Component {
         this.state = {
             screenings: [],
             toPrint: [],
-            id: 0,
-            date: "",
-            startTime: "",
-            movieID: 0,
-            screeningRoomNum: 0,
-            seats: [0],
-            max: 0,
-            amount: 0
+            roomSeats: []
         }
     }
 
@@ -29,7 +22,7 @@ export default class BuyTicketComponent extends React.Component {
                     axios.get("http://localhost:7777/movies/" + screening.movieID)
                         .then(res => {
                             const movie = res.data;
-        
+
                             const item = {
                                 id: screening.id,
                                 title: movie.title,
@@ -37,7 +30,7 @@ export default class BuyTicketComponent extends React.Component {
                                 startTime: screening.startTime,
                                 roomNum: screening.screeningRoomNum
                             }
-        
+
                             this.setState({ toPrint: [...this.state.toPrint, item] })
                         })
                 })
@@ -45,59 +38,38 @@ export default class BuyTicketComponent extends React.Component {
     }
 
     handleSubmit = (e) => {
-        e.preventDefault();
+        const screeningIndex = this.state.screenings.findIndex(tempScreening => tempScreening.id == e.target.screenings.value)
+        let screening = this.state.screenings[screeningIndex]
 
-        this.state.screenings.map((Screening) => {
-            if (Screening.id == e.target.screenings.value) {
-                axios.get("http://localhost:7777/screenings/" + Screening.id)
-                    .then(res => {
-                            this.setState( {
-                            id:e.target.screenings.value,
-                            date: res.data.date,
-                            startTime: res.data.startTime,
-                            movieID: res.data.movieID,
-                            screeningRoomNum: res.data.screeningRoomNum,
-                            seats: res.data.takenSeats,
-                            amount: res.data.soldTicketsNumber,
-                            max: res.data.availableTicketsNumber
-                        } )
-                    })
-            }
-        })
+        screening.takenSeats.push(e.target.tickets.value * 1)
+        screening.soldTicketsNumber++
+        screening.availableTicketsNumber--
 
-        const maxs = this.state.max
-        if (maxs != 0) {
-            let seats = this.state.seats;
-            let amount = this.state.amount;
-            let add = 0;
-            add = e.target.seat_num.value;
-            if (!seats.includes(add)) {
-                amount = amount + 1;
-                seats.push(add);
+        axios.put("http://localhost:7777/screenings/" + screening.id, screening)
+            .then(res => {
+                console.log(res)
+            })
+    }
+
+    screeningsOnChange = (e) => {
+        e.preventDefault()
+
+        const screening = this.state.screenings.find(tempScreening => tempScreening.id == e.target.value)
+
+        let Seats = [];
+        for (let i = 1; i <= screening.availableTicketsNumber + screening.soldTicketsNumber; i++) {
+            if (!screening.takenSeats.includes(i)) {
+                Seats.push(i);
             }
-            this.setState({ seats });
-            this.setState({ amount });
         }
-        this.state.screenings.map((Screening) => {
-            if (Screening.id == e.target.screenings.value) {
-                axios.put("http://localhost:7777/screenings/" + Screening.id, {
-                    id: this.state.id,
-                    date: this.state.date,
-                    startTime: this.state.startTime,
-                    movieID: this.state.movieID,
-                    screeningRoomNum: this.state.screeningRoomNum,
-                    soldTicketsNumber: this.state.amount,
-                    availableTicketsNumber: this.state.max,
-                    takenSeats: this.state.seats
-                })
-            }
-        })
+
+        this.setState({ roomSeats: Seats })
     }
 
     Screenings = () => {
 
         return (
-            <select name="screenings" multiple className="scrollTroughList">
+            <select name="screenings" multiple className="scrollTroughList" onChange={e => this.screeningsOnChange(e)}>
                 {
                     this.state.toPrint.map(item =>
                         <option value={item.id}>{item.date} - {item.startTime} - {item.title} - Room No.{item.roomNum}</option>
@@ -107,14 +79,28 @@ export default class BuyTicketComponent extends React.Component {
         );
     }
 
+    Tickets = () => {
+
+        return (
+            <select name="tickets" multiple className="scrollTroughList">
+                {
+                    this.state.roomSeats.map(item =>
+                        <option value={item}>{item}</option>
+                    )
+                }
+            </select>
+        );
+    }
+
     render() {
-        console.log(this.state.toPrint);
+
         return (
             <div>
                 <form onSubmit={e => this.handleSubmit(e)}>
-                    <this.Screenings />
-                    <label>Choose Seat number:</label><br />
-                    <input type="number" name="seat_num" /><br />
+                    <label>Choose screening:</label><br />
+                    <this.Screenings /><br />
+                    <label>Choose seat number:</label><br />
+                    <this.Tickets /><br />
                     <input type="submit" value="Buy" ></input>
                 </form>
             </div>
