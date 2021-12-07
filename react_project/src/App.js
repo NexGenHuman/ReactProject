b2bf424643d5eec77f82449bddad9dc6e7e6b043
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-12-07 22:45:08 +0100

    added basic styles

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -15,38 +15,40 @@ import EditMovieComponent from "./Components/EditMovieComponent";
 
 function App() {
   return (
-    <div className="App routerContainer">
-      <Router>
-        <div className="pageSelection">
-          <p><Link to="/">Home</Link></p>
-          <p><Link to="/addScreening">Add screening</Link></p>
-          <p><Link to="/addMovie">Add movie</Link></p>
-          <p><Link to="/addScreeningRoom">Add screening room</Link></p>
-          <p><Link to="/GetDate">Get date</Link></p>
-          <p><Link to="/BuyTicket">Buy Ticket</Link></p>
-          <p><Link to="/GetPopularity">How popular is the movie</Link></p>
-          <p><Link to="/DeleteScreening">Delete chosen screening</Link></p>
-          <p><Link to="/DeleteScreeningRoom">Delete chosen screening room</Link></p>
-          <p><Link to="/DeleteMovie">Delete chosen movie</Link></p>
-          <p><Link to="/EditScreening">Edit chosen screening</Link></p>
-          <p><Link to="/editMovie">Edit movie</Link></p>
-        </div>
+    <div className="App">
+      <div className="routerContainer">
+        <Router>
+          <div className="pageSelection">
+            <p><Link to="/">Home</Link></p>
+            <p><Link to="/addScreening">Add screening</Link></p>
+            <p><Link to="/addMovie">Add movie</Link></p>
+            <p><Link to="/addScreeningRoom">Add screening room</Link></p>
+            <p><Link to="/GetDate">Get date</Link></p>
+            <p><Link to="/BuyTicket">Buy Ticket</Link></p>
+            <p><Link to="/GetPopularity">How popular is the movie</Link></p>
+            <p><Link to="/DeleteScreening">Delete chosen screening</Link></p>
+            <p><Link to="/DeleteScreeningRoom">Delete chosen screening room</Link></p>
+            <p><Link to="/DeleteMovie">Delete chosen movie</Link></p>
+            <p><Link to="/EditScreening">Edit chosen screening</Link></p>
+            <p><Link to="/editMovie">Edit movie</Link></p>
+          </div>
 
-        <Routes>
-          <Route exact path="/" element={<HomeComponent />} />
-          <Route exact path="/addScreening" element={<AddScreeningComponent />} />
-          <Route exact path="/addMovie" element={<AddMovieComponent />} />
-          <Route exact path="/editMovie" element={<EditMovieComponent />} />
-          <Route exact path="/addScreeningRoom" element={<AddScreeningRoomComponent />} />
-          <Route exact path="/GetDate" element={<GetDateComponent />} />
-          <Route exact path="/BuyTicket" element={<BuyTicketComponent/>}/>
-          <Route exact path="/GetPopularity" element={<GetPopularity/>}/>
-          <Route exact path="/DeleteScreening" element={<DeleteScreeningComponent/>}/>
-          <Route exact path="/DeleteScreeningRoom" element={<DeleteScreeningRoomComponent/>}/>
-          <Route exact path="/DeleteMovie" element={<DeleteMovieComponent/>}/>
-          <Route exact path="/editScreening" element={<EditScreeningComponent />} />
-        </Routes>
-      </Router>
+          <Routes>
+            <Route exact path="/" element={<HomeComponent />} />
+            <Route exact path="/addScreening" element={<AddScreeningComponent />} />
+            <Route exact path="/addMovie" element={<AddMovieComponent />} />
+            <Route exact path="/editMovie" element={<EditMovieComponent />} />
+            <Route exact path="/addScreeningRoom" element={<AddScreeningRoomComponent />} />
+            <Route exact path="/GetDate" element={<GetDateComponent />} />
+            <Route exact path="/BuyTicket" element={<BuyTicketComponent />} />
+            <Route exact path="/GetPopularity" element={<GetPopularity />} />
+            <Route exact path="/DeleteScreening" element={<DeleteScreeningComponent />} />
+            <Route exact path="/DeleteScreeningRoom" element={<DeleteScreeningRoomComponent />} />
+            <Route exact path="/DeleteMovie" element={<DeleteMovieComponent />} />
+            <Route exact path="/editScreening" element={<EditScreeningComponent />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
